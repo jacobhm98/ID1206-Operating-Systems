2fbb0b7b843423bacb1bb9674142ba85c32fa9e7
Jacob Hedén Malm <jacmalm@kth.se>
2020-10-29 13:10:01 +0100

    clion bizness

---
+++ a_heap_a_stack/cmake-build-debug/CMakeFiles/a_heap_a_stack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_heap_a_stack.dir/stack.c.o"
+  "a_heap_a_stack"
+  "a_heap_a_stack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a_heap_a_stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
