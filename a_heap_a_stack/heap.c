17571a16d12e3cb0a63ebe4638ce4bd565f0802b
jacobhm98 <jacmalm@kth.se>
2020-11-04 21:30:48 +0100

    dont really know what went on lol

--- a_heap_a_stack/heap.c
+++ a_heap_a_stack/heap.c
@@ -5,17 +5,19 @@
 int main(){
     int pid = getpid();
 
-    char *heap = malloc(20);
-    *heap = 0x61;
-    printf("heap pointing to: 0x%x\n", *heap);
+    long *heap = (unsigned long*) calloc(40, sizeof(unsigned long));
+    printf("heap[2]: 0x%1x\n", heap[2]);
+    printf("heap[1]: 0x%1x\n", heap[1]);
+    printf("heap[0]: 0x%1x\n", heap[0]);
+    printf("heap[-1]: 0x%1x\n", heap[-1]);
+    printf("heap[-2]: 0x%1x\n", heap[-2]);
+
     free(heap);
-
-    char *foo = malloc(20);
-    *foo = 0x62;
-    printf("foo pointing to: 0x%x\n", *foo);
-
-    *heap = 0x63;
-    printf("or is it pointing to: 0x%x\n", *foo);
+    printf("heap[2]: 0x%1\n", heap[2]);
+    printf("heap[1]: 0x%1x\n", heap[1]);
+    printf("heap[0]: 0x%1x\n", heap[0]);
+    printf("heap[-1]: 0x%1x\n", heap[-1]);
+    printf("heap[-2]: 0x%1x\n", heap[-2]);
 
     back:
     printf("\n\n /proc/%d/maps\n\n",pid);
