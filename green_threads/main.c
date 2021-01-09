8a283644061e6f23af4f5860af1d9ca519eaf817
Jacob Hedén Malm <jacmalm@kth.se>
2021-01-09 22:09:46 +0300

    large number

--- green_threads/main.c
+++ green_threads/main.c
@@ -2,23 +2,26 @@
 #include "green.h"
 
 void *test_cond(void *arg);
+
 void *test_yield(void *arg);
+
 void first_small_test();
-void *counter_1(void*);
-void *counter_2(void*);
+
+void *increment_counter(void *arg);
+
 void test_race_cond();
 
-void *test_yield(void *arg){
-    int i = *(int*)arg;
+void *test_yield(void *arg) {
+    int i = *(int *) arg;
     int loop = 4;
-    while (loop > 0){
+    while (loop > 0) {
         printf("thread %d: %d\n", i, loop);
         loop--;
         green_yield();
     }
 }
 
-void first_small_test(){
+void first_small_test() {
     green_t g0, g1;
     int a0 = 0;
     int a1 = 1;
@@ -28,40 +31,43 @@ void first_small_test(){
     green_join(&g1, NULL);
     printf("done\n");
 }
+
 int flag = 0;
 green_cond_t cond;
-void *test_cond(void *arg){
+
+void *test_cond(void *arg) {
     green_cond_init(&cond);
-    int id = *(int*) arg;
+    int id = *(int *) arg;
     int loop = 100000;
-    while (loop > 0){
-        if (flag == id){
+    while (loop > 0) {
+        if (flag == id) {
             printf("thread %d: %d\n", id, loop);
             loop--;
             flag = (id + 1) % 2;
             green_cond_signal(&cond);
-        }
-        else{
+        } else {
             green_cond_wait(&cond);
         }
     }
 }
+
 int counter = 0;
-void *counter_1(void *arg){
-    for (int i = 0; i < 100000; ++i)
+
+void *increment_counter(void *arg) {
+    int loop = 10000000000000000;
+    while (loop > 0) {
         counter++;
-}
-void *counter_2(void *arg){
-    for (int i = 0; i < 100000; ++i)
-        counter++;
+        loop--;
+    }
+
 }
 
-void test_race_cond(){
+void test_race_cond() {
     green_t g0, g1;
     int a0 = 0;
     int a1 = 1;
-    green_create(&g0, counter_1, &a0);
-    green_create(&g1, counter_2, &a1);
+    green_create(&g0, increment_counter, &a0);
+    green_create(&g1, increment_counter, &a1);
     green_join(&g0, NULL);
     green_join(&g1, NULL);
 }
