37841987a512e9c46867bd0e91600eb509d7ec8a
Jacob Hedén Malm <jacmalm@kth.se>
2021-01-11 15:05:27 +0300

    ok

--- green_threads/green.c
+++ green_threads/green.c
@@ -61,6 +61,7 @@ void timer_handler(int sig){
     green_t *next = dequeue(&readyQueue);
     running = next;
 
+    write(1, "timer interrupt\n", strlen("timer interrupt\n"));
    // clock_t executionEnd = clock();
    // double time_spent = (double) (executionEnd - executionBegin)/CLOCKS_PER_SEC;
    // time_spent = time_spent * 1000;
