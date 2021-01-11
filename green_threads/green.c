266000f12b53c0b091358d2147bc1fefb6fc113c
Jacob Hedén Malm <jacmalm@kth.se>
2021-01-12 02:35:42 +0300

    signal print location

--- green_threads/green.c
+++ green_threads/green.c
@@ -210,11 +210,10 @@ void green_cond_wait(green_cond_t *cond, green_mutex_t *mutex) {
 }
 
 void green_cond_signal(green_cond_t *cond) {
-    assert(cond->waiting != NULL);
+    printf("waking up a thread from cv suspension\n");
     green_t *wake_up = dequeue(cond->waiting);
     assert(wake_up != NULL);
     enqueue(&readyQueue, wake_up);
-    printf("waking up a thread from cv suspension\n");
 }
 
 int green_mutex_init(green_mutex_t *mutex) {
