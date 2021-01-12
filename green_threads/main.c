1df820273fc58c63f95f988f1bdb7dc703f75f9c
Jacob Hedén Malm <jacmalm@kth.se>
2021-01-12 03:52:30 +0300

    producer/consumer

--- green_threads/main.c
+++ green_threads/main.c
@@ -2,9 +2,17 @@
 #include "green.h"
 
 void *test_cond(void *arg);
+
 void *test_yield(void *arg);
+
 void *increment_counter(void *arg);
+
 void *mutex_test(void *arg);
+
+void *consumer(void *arg);
+
+void *producer(void *arg);
+
 void first_small_test();
 
 int flag = 0;
@@ -12,8 +20,47 @@ green_cond_t cond;
 green_mutex_t mutex;
 long counter = 0;
 
-void *test_yield(void *arg){
-    int i = *(int*)arg;
+
+green_cond_t empty;
+green_cond_t full;
+int resource = 0;
+#define EMPTY 0
+#define FULL 1
+
+void *producer(void *arg) {
+    while (1) {
+        green_mutex_lock(&mutex);
+        if (resource == EMPTY) {
+            //produce
+            resource = FULL;
+            printf("PRODUCE");
+            green_cond_signal(&full);
+        }
+        else{
+            green_cond_wait(&empty, &mutex);
+        }
+        green_mutex_unlock(&mutex);
+    }
+}
+
+void *consumer(void *arg){
+    while(1){
+        green_mutex_lock(&mutex);
+        if (resource == FULL){
+            //consume
+            resource = EMPTY;
+            printf("CONSUMING\n");
+            green_cond_signal(&empty);
+        }
+        else{
+            green_cond_wait(&full, &mutex);
+        }
+        green_mutex_unlock("&mutex");
+    }
+}
+
+void *test_yield(void *arg) {
+    int i = *(int *) arg;
     int loop = 4;
     while (loop > 0) {
         printf("thread %d: %d\n", i, loop);
@@ -28,8 +75,8 @@ void first_small_test() {
     int a1 = 1;
     int a2 = 2;
     int a3 = 3;
-    green_create(&g0, test_cond, &a0);
-    green_create(&g1, test_cond, &a1);
+    green_create(&g0, producer, &a0);
+    green_create(&g1, consumer, &a1);
     //green_create(&g2, mutex_test, &a2);
     //green_create(&g3, mutex_test, &a3);
     green_join(&g0, NULL);
@@ -38,26 +85,28 @@ void first_small_test() {
     //green_join(&g3, NULL);
     printf("value of counter %d\n", counter);
 }
-void *mutex_test(void *arg){
-    int id = *(int*) arg;
+
+void *mutex_test(void *arg) {
+    int id = *(int *) arg;
     int loop = 1000;
     green_mutex_lock(&mutex);
-    while (loop > 0){
+    while (loop > 0) {
         loop--;
         printf("thread %d is here\n", id);
     }
     green_mutex_unlock(&mutex);
     return NULL;
 }
-void *increment_counter(void *arg){
-    int id = *(int*) arg;
+
+void *increment_counter(void *arg) {
+    int id = *(int *) arg;
     int loop = 100000000;
-    while (loop > 0){
-            //printf("thread %d: %d\n", id, loop);
-            loop--;
-            green_mutex_lock(&mutex);
-            counter++;
-            green_mutex_unlock(&mutex);
+    while (loop > 0) {
+        //printf("thread %d: %d\n", id, loop);
+        loop--;
+        green_mutex_lock(&mutex);
+        counter++;
+        green_mutex_unlock(&mutex);
     }
 }
 
@@ -80,7 +129,8 @@ void *test_cond(void *arg) {
 }
 
 int main() {
-    green_cond_init(&cond);
+    green_cond_init(&empty);
+    green_cond_init(&full);
     green_mutex_init(&mutex);
     first_small_test();
     return 0;
